854b642c77695455237bca5047998d1bee5d0f78
PraveenAchary <praveenvishwabramham@gmail.com>
2026-08-23 21:00:27 +0530

    Update settings.py

--- backend/cppdebugger/settings.py
+++ backend/cppdebugger/settings.py
@@ -46,7 +46,14 @@ INSTALLED_APPS = [
     'Agent2',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://cppdebugger.vercel.app",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
